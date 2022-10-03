6ec81a4d516cf4ed5e74fd3f0dd25837214191ce
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-10-03 18:14:19 -0400

    basic component

--- expense-app/src/App.js
+++ expense-app/src/App.js
@@ -1,9 +1,13 @@
-
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
+    // return html code inside the function
+    // jsx is a mix of html and javascript
     <div className="App">
       <h2>React 18</h2>
+      {/*Imported custom component*/}
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ expense-app/src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+// component are written using JS functions
+function ExpenseItem() {
+    // eslint-disable-next-line jsx-a11y/heading-has-content
+    return (
+    <><div>
+    <div>Date</div>
+    </div>
+    <h2>Title</h2>
+    <div>Amount</div></>
+    
+    );
+}
+
+// export component
+export default ExpenseItem;
