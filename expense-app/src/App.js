3b8818279e493792e4a1f8f83b0a32283b682326
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-10-07 19:19:44 -0400

    props added to pass data

--- expense-app/src/App.js
+++ expense-app/src/App.js
@@ -1,13 +1,25 @@
 import ExpenseItem from './components/ExpenseItem';
+import './index.css';
 
 function App() {
+  // components are reusible to pass promps data
+  const expenses =[
+    {id:'e1',title: 'STM Pass', amount: 60, date: new Date(2022, 2, 12)},
+    {id:'e2',title: 'Phone Bill', amount: 55, date: new Date(2022,7,14)},
+    {id:'e3',title: 'Rent',amount: 900, date: new Date(2022,7,20)},
+    {id:'e4',title: 'Subscription', amount: 20, date: new Date(2022, 6, 20)}
+  ]
   return (
     // return html code inside the function
     // jsx is a mix of html and javascript
     <div className="App">
-      <h2>React 18</h2>
+      <h2>expense app</h2>
       {/*Imported custom component*/}
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+      
     </div>
   );
 }
--- expense-app/src/components/ExpenseItem.js
+++ expense-app/src/components/ExpenseItem.js
@@ -1,15 +1,22 @@
+import './ExpenseItem.css';
+
 // component are written using JS functions
-function ExpenseItem() {
+// hold the value for custom elements with keys
+function ExpenseItem(props) {
     // eslint-disable-next-line jsx-a11y/heading-has-content
     return (
-    <><div>
-    <div>Date</div>
-    </div>
-    <h2>Title</h2>
-    <div>Amount</div></>
-    
+    <div className="expense-item">
+        {/* output date as a string */}
+        <div>{props.date.toISOString}</div> 
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
     );
 }
+
+
 
 // export component
 export default ExpenseItem;
