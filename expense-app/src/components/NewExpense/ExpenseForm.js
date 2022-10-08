0ceec5b295d741c46f8950cf5cfb5bedb14400cd
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-10-08 17:45:55 -0400

    form for user input being created

--- expense-app/src/components/NewExpense/ExpenseForm.js
+++ expense-app/src/components/NewExpense/ExpenseForm.js
@@ -4,46 +4,63 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () =>{
     // states to store user input 
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
     // other way to group all 3 state object to manage to piece of state
-    const [userInput, setUserInput] =useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: '',
-    });
+    // const [userInput, setUserInput] =useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: '',
+    // });
 
     // handler function for title
     const titleChangeHandler = (event) =>{
-        //setEnteredTitle(event.target.value);
-        setUserInput({
-            // take the object and pull all keyvalue pairs \\ other value are not throwned away
-            ...userInput, // override entered title
-            enteredTitle: event.target.value,
-        });
-    }
+        setEnteredTitle(event.target.value);
+        // setUserInput({
+        //     // take the object and pull all keyvalue pairs \\ other value are not throwned away
+        //     ...userInput, // override entered title
+        //     enteredTitle: event.target.value,
+        // });
+        // when state function is dependent of previous state
+        // pass another function to autmatically be excecuted by react to 
+        // react schedule state updates \\ using outdated state snapshot
+        // setUserInput((prevState) =>{
+        //     return{...prevState, enteredTitle: event.target.value};
+        // });
+    };
     // handler function for amount of money
     const amountChangeHandler = (event) =>{
-       // setEnteredAmount(event.target.value);
-       setUserInput({
-        // take the object and pull all keyvalue pairs \\ other value are not throwned away
-        ...userInput, // override entered title
-        enteredAmount: event.target.value,
-    });
+       setEnteredAmount(event.target.value);
+    //    setUserInput({
+    //     // take the object and pull all keyvalue pairs \\ other value are not throwned away
+    //     ...userInput, // override entered title
+    //     enteredAmount: event.target.value,
+    // });
     }
     // handler function for date
     const dateChangeHandler = (event) =>{
-      //  setEnteredDate(event.target.value);
-      setUserInput({
-        // take the object and pull all keyvalue pairs \\ other value are not throwned away
-        ...userInput, // override entered title
-        enteredDate: event.target.value,
-    });
+       setEnteredDate(event.target.value);
+    //   setUserInput({
+    //     // take the object and pull all keyvalue pairs \\ other value are not throwned away
+    //     ...userInput, // override entered title
+    //     enteredDate: event.target.value,
+    // });
     }
 
+    const submitHandler = (event) =>{
+        event.preventDefault(); // prevent default of the request being send
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
+    };
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
