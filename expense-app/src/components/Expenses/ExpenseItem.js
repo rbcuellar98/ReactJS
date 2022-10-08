b37fa77c957e2a546b5d216f24ecc3c03656f05d
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-10-08 19:19:09 -0400

    removed redundant code

--- expense-app/src/components/Expenses/ExpenseItem.js
+++ expense-app/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
@@ -6,14 +6,7 @@ import './ExpenseItem.css';
 // component are written using JS functions
 // hold the value for custom elements with keys
 const ExpenseItem = (props) => {
-    // react hook
-    const [title, setTitle] = useState(props.title);
-    // event listerner in JSX structure
-    const clickHandler = () =>{
-        // state of the title being changed
-        setTitle('Updated');
-        console.log(title);
-    };
+    
 
     // eslint-disable-next-line no-lone-blocks
     // eslint-disable-next-line jsx-a11y/heading-has-content
@@ -23,10 +16,9 @@ const ExpenseItem = (props) => {
             {/* component tree nested with each other */}
             <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
     </Card>
     );
 }
