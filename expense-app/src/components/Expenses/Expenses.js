9fa2fc04f38bf1f00d2118cf76539dffe44785d6
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-10-08 19:08:25 -0400

    filtering year

--- expense-app/src/components/Expenses/Expenses.js
+++ expense-app/src/components/Expenses/Expenses.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpenseFilter from './ExpenseFilter';
+
 
 const Expenses =(props) =>{
+    const [filteredYear, setFilteredYear] = useState('2022');
+    const filterChangeHandler = (selectedYear) =>{
+        setFilteredYear(selectedYear);
+    }
     return (
+    <div>
         <Card className='expenses'>
             {/*Imported custom component*/}
+      <ExpenseFilter selected = { filteredYear} onChangeFilter = {filterChangeHandler}/>
       <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
       <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
       <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
       <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
       
         </Card>
+    </div>
     );
 }
 
