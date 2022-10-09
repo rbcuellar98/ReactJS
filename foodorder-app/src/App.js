456b55826382a67a542ec5101667e0a1629bc2ef
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-10-08 21:52:22 -0400

    header added to project

--- foodorder-app/src/App.js
+++ foodorder-app/src/App.js
@@ -1,11 +1,11 @@
-
-import './App.css';
+import { Fragment } from 'react';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Fragment>
+      <Header></Header>
+    </Fragment>
   );
 }
 
