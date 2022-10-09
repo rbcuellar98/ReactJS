0ea07df5366ad81980a2a3a412921dcea6cbebd8
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-10-08 21:14:53 -0400

    errormodal added to the project

--- usercreation-app/src/components/Users/AddUser.js
+++ usercreation-app/src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) =>{
     // array destructuring pulling the elements out of the array 
@@ -30,17 +31,21 @@ const AddUser = (props) =>{
 
 
     return(
-    <Card className={classes.input}>
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username"> Username</label>
-            <input id ="username" type="text" value = {enteredUsername} onChange={usernameChangeHandler}></input>
+    <div>
+        <ErrorModal title="An error occured!" message="Something went wrong!"/>
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username"> Username</label>
+                <input id ="username" type="text" value = {enteredUsername} onChange={usernameChangeHandler}></input>
 
-            <label htmlFor="age"> Age (Years)</label>
-            <input id ="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
+                <label htmlFor="age"> Age (Years)</label>
+                <input id ="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
 
-            <Button type="submit">Add User</Button>
-        </form>
-    </Card>
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
+    </div>
+    
     );
 };
 
