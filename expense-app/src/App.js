11f9d49971e88c9086d51a5579a15d46a5ce4622
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-10-10 15:59:35 -0400

    Added expenses filtering

--- expense-app/src/App.js
+++ expense-app/src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Expenses from './components/Expenses/Expenses';
 import './index.css';
 import NewExpense from './components/NewExpense/NewExpense';
 
+
+const INITIAL_EXPENSES =[
+  {id:'e1',title: 'STM Pass', amount: 60, date: new Date(2022, 2, 12)},
+  {id:'e2',title: 'Phone Bill', amount: 55, date: new Date(2022,7,14)},
+  {id:'e3',title: 'Rent',amount: 900, date: new Date(2022,7,20)},
+  {id:'e4',title: 'Subscription', amount: 20, date: new Date(2022, 6, 20)},
+];
+
 const App = () => {
   // components are reusible to pass promps data
-  const expenses =[
-    {id:'e1',title: 'STM Pass', amount: 60, date: new Date(2022, 2, 12)},
-    {id:'e2',title: 'Phone Bill', amount: 55, date: new Date(2022,7,14)},
-    {id:'e3',title: 'Rent',amount: 900, date: new Date(2022,7,20)},
-    {id:'e4',title: 'Subscription', amount: 20, date: new Date(2022, 6, 20)},
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+  
 
   const addExpenseHandler = expense =>{
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return[expense, ...prevExpenses];
+    });
   }
     // eslint-disable-next-line no-lone-blocks
     {/*Alternative to JSX using React object*/}
